64b39433918f460ea48703b3efe43926cbc106c3
Pinhas <piniziv8@gmail.com>
2020-04-20 09:31:40 +0300

    anither change

--- ComicBook.java
+++ ComicBook.java
@@ -1,5 +1,37 @@
 package homeWork4;
 
-public class ComicBook {
+public class ComicBook extends LibraryBook {
+
+	private int numOfCopies;
+
+	public ComicBook() {
+		super();
+		this.numOfCopies = 10;
+	}
+
+	public ComicBook(int numOfCopies) {
+		super();
+		this.numOfCopies = numOfCopies;
+	}
+
+	public int getNumOfCopies() {
+		return numOfCopies;
+	}
+
+	public void setNumOfCopies(int numOfCopies) {
+		if (numOfCopies < 0) {
+			this.numOfCopies = 0;
+		} else {
+			this.numOfCopies = numOfCopies;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Book: " + this.getNameOfBook() 
+		+ " Authr: " + this.getNameOfAuthor() + " Pages: " + this.getNumOfPages()
+		+ " Num of copies:" + this.getNumOfCopies();
+
+	}
 
 }
